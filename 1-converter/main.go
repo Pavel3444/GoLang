7b774e9819d7d2045ca11bee959ffdbe55c30e8a
Pavel3444 <burikov.pavel3444@yandex.ru>
2025-04-06 20:51:48 +0300

    1-converter: 2

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,5 +7,27 @@ func main() {
 	const USDtoRUB = 75
 	const EURtoRUB = USDtoRUB / USDtoEUR
 
-	fmt.Println("EUR to RUB exchange rate:", EURtoRUB)
+	currency, targetCurrency, amount := userInputData()
+
+	fmt.Println("your data:", currency, targetCurrency, amount)
+
+}
+
+func userInputData() (string, string, float64) {
+	var currency string
+	var targetCurrency string
+	var amount float64
+
+	fmt.Println("Введите код валюты для конвертирования: ")
+	fmt.Scan(&currency)
+	fmt.Println("Введите код целевой валюты для конвертирования: ")
+	fmt.Scan(&targetCurrency)
+	fmt.Println("Введите сумму конвертирования: ")
+	fmt.Scan(&amount)
+
+	return currency, targetCurrency, amount
+}
+
+func convert(currency, targetCurrency string, amount float64) float64 {
+	return amount
 }
