7ebf4666150b0f6ee98441b4ae9518b651d9f307
Pavel3444 <burikov.pavel3444@yandex.ru>
2025-04-22 20:07:59 +0300

    3-struct: init

---
+++ 3-bin/main.go
@@ -0,0 +1,33 @@
+package __bin
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Bin struct {
+	Id         string
+	Private    bool
+	CreateTime time.Time
+	Name       string
+}
+
+type BinList struct {
+	Bins []Bin
+}
+
+func (list *BinList) NewBin(private bool, name string) *Bin {
+	bin := Bin{
+		Private:    private,
+		Name:       name,
+		Id:         uuid.New().String(),
+		CreateTime: time.Now(),
+	}
+	list.Bins = append(list.Bins, bin)
+	return &list.Bins[len(list.Bins)-1]
+}
+
+func NewBinList() *BinList {
+	return &BinList{}
+}
