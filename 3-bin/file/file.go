8d80e16513f517ec36dfd5d7c1ad6b7e4424c8e9
Pavel3444 <burikov.pavel3444@yandex.ru>
2025-04-29 15:20:47 +0300

    3-struct: fix ReadFile

--- 3-bin/file/file.go
+++ 3-bin/file/file.go
@@ -2,44 +2,24 @@ package file
 
 import (
 	"fmt"
-	"io"
 	"mime"
 	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 )
 
 func ReadFile(source string) (data []byte, contentType string, err error) {
-	u, parseErr := url.Parse(source)
-	if parseErr == nil && (u.Scheme == "http" || u.Scheme == "https") {
-		resp, err := http.Get(source)
-		if err != nil {
-			return nil, "", fmt.Errorf("HTTP GET error: %w", err)
-		}
-		defer resp.Body.Close()
 
-		if resp.StatusCode != http.StatusOK {
-			return nil, "", fmt.Errorf("HTTP GET returned status %s", resp.Status)
-		}
-
-		data, err = io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, "", fmt.Errorf("reading HTTP body: %w", err)
-		}
-	} else {
-		data, err = os.ReadFile(source)
-		if err != nil {
-			return nil, "", fmt.Errorf("os.ReadFile error: %w", err)
-		}
+	data, err = os.ReadFile(source)
+	if err != nil {
+		return nil, "", fmt.Errorf("os.ReadFile error: %w", err)
 	}
 
 	contentType = http.DetectContentType(data)
-	if u == nil || u.Scheme == "" {
-		if ext := filepath.Ext(source); ext != "" {
-			if mt := mime.TypeByExtension(ext); mt != "" {
-				contentType = mt
-			}
+
+	if ext := filepath.Ext(source); ext != "" {
+		if mt := mime.TypeByExtension(ext); mt != "" {
+			contentType = mt
 		}
 	}
 
